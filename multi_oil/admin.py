97f52e075b202b9b3eba44f87d47013d5053cc6d
XJlEBYWEK <spainszlojob@gmail.com>
2021-10-20 18:07:13 +0300

    add tags

--- multi_oil/admin.py
+++ multi_oil/admin.py
@@ -1,14 +1,36 @@
 from django.contrib import admin
 from . import models
 
-
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('name', 'date_add')
 
+    fieldsets = (
+        ('Новость', {
+            'fields': ('name', 'body', 'data')
+        }),
+        ('Изображение', {
+            'fields': ('img', 'img_alt', 'img_title',)
+        }),
+        ('Страница', {
+            'fields': ('name_url', 'page_title', 'page_description', 'page_keywords')
+        }),)
+
 
 class StocksAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date_add', 'name_url', 'date_work','date_add')
-    fields = ['name', 'img', 'date_work', 'body', 'name_url']
+    list_display = ('name', 'date_add', 'name_url', 'date_work', 'date_add')
+
+    fieldsets = (
+        ('Акция', {
+            'fields': ('name', 'body', 'date_work')
+        }),
+        ('Изображение', {
+            'fields': ('img', 'img_alt', 'img_title',)
+        }),
+        ('Страница', {
+            'fields': ('name_url', 'page_title', 'page_description', 'page_keywords')
+        }),)
+
+
 
 
 admin.site.register(models.News, NewsAdmin)
