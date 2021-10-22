a2c8006fd9bc6ee022c3b9501d476a64ada00608
XJlEBYWEK <spainszlojob@gmail.com>
2021-10-22 13:21:43 +0300

    update

--- multi_oil/serializers.py
+++ multi_oil/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import News, Stocks
 
-base_url = "https://multioil.com.ua"
+base_url = "http://127.0.0.1:8000"
 
 
 class NewsSerializer(serializers.ModelSerializer):
